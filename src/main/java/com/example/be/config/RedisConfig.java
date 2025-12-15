24b8b9de17d3850cc4b62c379f8b886d5bc3a26d
sunJ0120 <sspure1214@gmail.com>
2025-12-15 17:49:52 +0900

    feat: redis 직렬화 설정 추가

--- src/main/java/com/example/be/config/RedisConfig.java
+++ src/main/java/com/example/be/config/RedisConfig.java
@@ -1,4 +1,51 @@
 package com.example.be.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import tools.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
 public class RedisConfig {
+
+  @Value("${spring.data.redis.host}")
+  private String hostName;
+  @Value("${spring.data.redis.port}")
+  private int port;
+
+  @Bean
+  LettuceConnectionFactory connectionFactory() {
+    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+    redisStandaloneConfiguration.setHostName(hostName);
+    redisStandaloneConfiguration.setPort(port);
+
+    return new LettuceConnectionFactory(redisStandaloneConfiguration);
+  }
+
+  @Bean
+  public ObjectMapper objectMapper() {
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    return objectMapper;
+  }
+
+  @Bean
+  RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory,
+      ObjectMapper objectMapper) {
+
+    RedisTemplate<String, Object> template = new RedisTemplate<>();
+    template.setConnectionFactory(connectionFactory);
+
+    template.setKeySerializer(new StringRedisSerializer());
+    template.setValueSerializer(new GenericJacksonJsonRedisSerializer(objectMapper));
+
+    template.afterPropertiesSet();
+    return template;
+  }
 }
