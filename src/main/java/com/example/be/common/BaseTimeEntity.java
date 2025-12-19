6c5452cf5177862aa55d6ac957108a371abe195b
sunJ0120 <sspure1214@gmail.com>
2025-12-19 23:57:10 +0900

    feat: 기본이 되는 베이스 엔티티 생성

--- src/main/java/com/example/be/common/BaseTimeEntity.java
+++ src/main/java/com/example/be/common/BaseTimeEntity.java
@@ -1,4 +1,17 @@
 package com.example.be.common;
 
-public class BaseTimeEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+  @CreatedDate
+  @Column(name = "created_at", updatable = false)
+  private LocalDateTime createdAt;
 }
