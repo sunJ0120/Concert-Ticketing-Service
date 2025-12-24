25ded372858fff56deb6e4551ea463fb02110b82
sunJ0120 <sspure1214@gmail.com>
2025-12-24 14:10:16 +0900

    feat: 에러 응답 정의 완료

--- src/main/java/com/example/be/common/exception/ErrorResponse.java
+++ src/main/java/com/example/be/common/exception/ErrorResponse.java
@@ -1,4 +1,8 @@
 package com.example.be.common.exception;
 
-public class ErrorRespons {
+public record ErrorResponse(String code, String message) {
+
+  public static ErrorResponse of(ErrorCode errorCode) {
+    return new ErrorResponse(errorCode.getCode(), errorCode.getMessage());
+  }
 }
