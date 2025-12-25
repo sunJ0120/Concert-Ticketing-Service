35a721163802a1a1889b9140348eee6d6e50befb
sunJ0120 <sspure1214@gmail.com>
2025-12-25 23:28:50 +0900

    feat: test code에서 사용을 위해 repository 미리 생성

--- src/main/java/com/example/be/concert/repository/BuildingRepository.java
+++ src/main/java/com/example/be/concert/repository/BuildingRepository.java
@@ -1,4 +1,6 @@
 package com.example.be.concert.repository;
 
-public interface BuildingRepository {
-}
+import com.example.be.concert.domain.Building;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BuildingRepository extends JpaRepository<Building, Long> {}
